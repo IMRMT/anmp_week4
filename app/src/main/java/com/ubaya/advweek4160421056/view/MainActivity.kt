80a259cc32a2fe33bbae65c5a41967b248317011
IMRMT <michaeltimonuli@gmail.com>
2024-03-21 13:30:20 +0700

    latest

--- app/src/main/java/com/ubaya/advweek4160421056/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/advweek4160421056/view/MainActivity.kt
@@ -2,11 +2,21 @@ package com.ubaya.advweek4160421056.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ubaya.advweek4160421056.R
+import com.ubaya.advweek4160421056.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.main_navigation)
+                as NavHostFragment).navController
     }
 }
